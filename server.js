dbe995edc81e958568af1e092aaae40441e70264
Kawthara <kawtharr.m.ahmad@gmail.com>
2025-09-02 16:35:36 +0300

    editing cors to receive from frontend

--- server.js
+++ server.js
@@ -1,46 +1,51 @@
 // imports
-const express = require('express')
-require('dotenv').config()
-const path = require('path')
-const cors = require('cors')
+const express = require("express")
+require("dotenv").config()
+const path = require("path")
+const cors = require("cors")
 
 // Initialize app
 const app = express()
 
 // Database Configuration
-const mongoose = require('./config/db')
+const mongoose = require("./config/db")
 
 // set Port Configuration
 const port = process.env.PORT ? process.env.PORT : 3000
 
 // Require MiddleWares
-app.use(cors())
-const morgan = require('morgan')
+app.use(
+  cors({
+    origin: "https://the-book-fair.surge.sh",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+)
+const morgan = require("morgan")
 
 // use MiddleWares
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use(morgan('dev'))
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(morgan("dev"))
+app.use(express.static(path.join(__dirname, "public")))
 
 // Root Route
-app.get('/', (req, res) => {
-  res.send('Your app is connected . . . ')
+app.get("/", (req, res) => {
+  res.send("Your app is connected . . . ")
 })
 
 // Require Routers
-const authRouter = require('./routes/authRouter')
-const userRouter = require('./routes/userRouter')
-const fairRouter = require('./routes/fairRouter')
-const ticketRouter = require('./routes/ticketRouter')
-const bookingRouter = require('./routes/bookingRouter')
+const authRouter = require("./routes/authRouter")
+const userRouter = require("./routes/userRouter")
+const fairRouter = require("./routes/fairRouter")
+const ticketRouter = require("./routes/ticketRouter")
+const bookingRouter = require("./routes/bookingRouter")
 
 // use Routers
-app.use('/auth', authRouter)
-app.use('/profile', userRouter)
-app.use('/fairs', fairRouter)
-app.use('/tickets', ticketRouter)
-app.use('/bookings', bookingRouter)
+app.use("/auth", authRouter)
+app.use("/profile", userRouter)
+app.use("/fairs", fairRouter)
+app.use("/tickets", ticketRouter)
+app.use("/bookings", bookingRouter)
 
 // Listener
 app.listen(port, () => {
